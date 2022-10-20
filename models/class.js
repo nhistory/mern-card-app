fe64f54d9b49ce1755517d35aa11838e7408931c
nhistory <sehwan.lee.dev@gmail.com>
2022-10-20 00:03:31 -0300

    Fix schema and add .env

--- models/class.js
+++ models/class.js
@@ -1,14 +1,30 @@
-const mongoose = require('mongoose')
-const { Schema } = mongoose //destructuring => const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const { Schema } = mongoose; //destructuring => const Schema = mongoose.Schema
 
 // define the schema
-const classSchema = new Schema({ 
-    type: String,
-    schedule: String,
-    startDate: String,
+const studentSchema = new Schema({
+  name: String,
+  motherName: String,
+  phone: String,
+  address: String,
+});
+
+const teacherSchema = new Schema({
+  name: String,
+  email: String,
+  phone: String,
+  image: String,
+});
+
+const classSchema = new Schema({
+  type: String,
+  schedule: String,
+  startDate: String,
+  teacher: teacherSchema,
+  student: [studentSchema],
 });
 
 //generate the model from the schema and export for use elsewhere
-const Class = mongoose.model('Class', classSchema)
+const Class = mongoose.model('Class', classSchema);
 
-module.exports = Class
+module.exports = Class;
