0006742ed74b6f55d36b901e42c99ee9608fc5d6
Sehwan Lee <sehwan.lee.dev@gmail.com>
2022-11-17 15:11:23 -0400

    Added client to project and added concurrently

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+import Main from './components/Main';
+import SignIn from './components/SignIn';
+import Footer from './components/Footer';
+
+import './css/app.css';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <NavBar />
+      <div id="main-content">
+        {/* <SignIn /> */}
+        <Main />
+      </div>
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;
