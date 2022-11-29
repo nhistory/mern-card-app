71f9b7c9cd4ce8627991054d7795393f05ab3198
nhistory <sehwan.lee.dev@gmail.com>
2022-11-29 17:10:05 -0400

    change express static path 'client/build'

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(logger('dev'));
 app.use(express.json()); // parses the body from the request
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // direct the incoming request to a particular router
 // based on the url path
